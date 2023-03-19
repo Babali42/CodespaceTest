c60de609d7c86c6553d7f8ed91aa8f68aa3d12c1
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2023-03-19 20:04:09 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace testc;
+
+public class TestClass{
+    public static void Main(){
+    // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World 2!");
+    }
+
+    [Test]
+    public void Should(){
+        Assert.IsTrue(true);
+    }
+}
